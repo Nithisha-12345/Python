eef67fe194024db997b015e88a0b961cee5e86cc
nithisha saidhu <nithishasaidhu2005@gmail.com>
2025-10-28 08:10:50 +0530

    message

---
+++ Conditinal-statements/if-statement.py
@@ -0,0 +1,45 @@
+#if statement
+a = 200
+b = 500
+if b > a:
+    print("b is greater than a")
+
+
+p = 1000
+q = 1000
+if p == q:
+    print("both p and q are equal")
+
+
+
+#to check a number is +ve or not
+num = 100
+if num > 0:
+    print("The number is positive")
+
+
+
+#multiple if block using print statements
+marks = 20
+if marks >= 15:
+    print("You are pass")
+    print("You can fail")
+    print("You are below avg")
+
+#using boolean
+good_girl= True
+if good_girl:
+    print("Nithu")
+
+
+#to check if num is even or odd
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("The number is even.")
+if num % 2 != 0:
+    print("The number is odd.")
+
+
+
+
+
