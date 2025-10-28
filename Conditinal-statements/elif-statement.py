d94679d9206ea5ad685d2d6d9bc4d20f506d7c45
nithisha saidhu <nithishasaidhu2005@gmail.com>
2025-10-28 08:11:58 +0530

    message

---
+++ Conditinal-statements/elif-statement.py
@@ -0,0 +1,55 @@
+#Elif Statement
+x = 33
+y = 33
+if y > x:
+    print("y is greater than x")
+elif x == y:
+    print("x and x are equal")
+
+
+
+#Testing multiple conditions:
+score = 75
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+
+
+#another example
+age = 25
+
+if age < 13:
+    print("You are a child")
+elif age < 20:
+    print("You are a teenager")
+elif age < 65:
+    print("You are an adult")
+elif age >= 65:
+    print("You are a senior")
+
+
+
+#another example
+day = 3
+
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+elif day == 4:
+    print("Thursday")
+elif day == 5:
+    print("Friday")
+elif day == 6:
+    print("Saturday")
+elif day == 7:
+    print("Sunday")
+
